bb9123b5742c5244521c912fe956d75961118386
Jackie Harnish <jackieleighh@gmail.com>
2025-04-08 15:03:57 -0400

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlgXrTBT5lSMA3hFKePPA9gY57i_yH91Q",
+  authDomain: "sj-pw-validation.firebaseapp.com",
+  projectId: "sj-pw-validation",
+  storageBucket: "sj-pw-validation.firebasestorage.app",
+  messagingSenderId: "1024881198428",
+  appId: "1:1024881198428:web:b4d0ab3ced2d35940ced99"
+};
+
 function App() {
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
